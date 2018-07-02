2636e3bd9fe046d685a311e8e802a5959db3b214
cristiano alex dos santos <cristiano_alex@live.com>
2018-07-02 01:19:23 -0300

    Adicionar arquivos de projeto.

---
+++ exercicios/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var multiplo3 =
+                from inteiro in num
+                where
+                inteiro % 3 == 0
+                select inteiro;
+
+            var outroJeito =
+                num.Where(inteiro => inteiro % 3 == 0)
+                .Select(inteiro => inteiro);
+
+            foreach (var inteiro in multiplo3)
+            {
+                Console.WriteLine(inteiro);
+            }
+            Console.ReadKey();
+        }
+    }
+}
